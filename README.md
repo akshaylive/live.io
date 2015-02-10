c3e391ee04b1ed32b1922306a6e76d39d542f8f5
Akshaya Shanbhogue <shanbhog@usc.edu>
2015-02-10 12:38:36 -0800

    + methods + updated readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# live.io
+LivE.io
+=========
+
 Live.io combines the power of the new Express 4.0, socket.io and passport! Empower your sockets!
+
+## Installation
+
+  npm install live.io --save
+
+## Usage
+
+  replace require('express') with require('live.io')
+
+  Add your socket route using the socket method.
+  app.route('/').socket(function(req, res){
+    // req.body contains socket data
+    // res.emit can emit data to socket.
+    // res.json is a helper method to return json data.
+  });
+
+## Tests
+
+  grunt test
+
+## Contributing
+
+In lieu of a formal styleguide, take care to maintain the existing coding style.
+Add unit tests for any new or changed functionality. Lint and test your code.
+
+## Release History
+
+* 0.0.1 Initial release
--- index.js
+++ index.js
@@ -3,10 +3,46 @@
  *
  * Created by Akshaya Shanbhogue on 2/10/2015.
  */
-require("express");
-require("socket.io");
-require("passport.socketio");
-
-module.exports = {
-
+var http = require('http');
+if(http.METHODS) {
+    // TODO - check if socket already exists
+    http.METHODS.push('socket');
 }
+else {
+    http.METHODS= [
+        'get',
+        'post',
+        'put',
+        'head',
+        'delete',
+        'socket', // ADDED NEW METHOD "socket"
+        'options',
+        'trace',
+        'copy',
+        'lock',
+        'mkcol',
+        'move',
+        'purge',
+        'propfind',
+        'proppatch',
+        'unlock',
+        'report',
+        'mkactivity',
+        'checkout',
+        'merge',
+        'm-search',
+        'notify',
+        'subscribe',
+        'unsubscribe',
+        'patch',
+        'search',
+        'connect'
+    ];
+}
+
+
+express = require("express");
+socketio = require("socket.io");
+passportio = require("passport.socketio");
+
+module.exports = express;
