8fdf2281f0cbed74688f9a00dd82b4e420ca2f87
Akshaya Shanbhogue <shanbhog@usc.edu>
2015-02-10 10:59:53 -0800

    + Dependencies + index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Combine Express, Socket and Passport
+ *
+ * Created by Akshaya Shanbhogue on 2/10/2015.
+ */
+require("express");
+require("socket.io");
+require("passport.socketio");
+
+module.exports = {
+    
+}
