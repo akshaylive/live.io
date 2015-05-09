c445bdc7b1e0b16529707108b22e2362b2a52077
Akshaya Shanbhogue <shanbhog@usc.edu>
2015-05-09 13:35:29 -0700

    + WSS

--- index.js
+++ index.js
@@ -141,15 +141,15 @@ module.exports = function(options){
         io.sockets.on('connection', function (socket) {
             var req = socket.client.request;
 
-            makeRequestResponse(req,req.res);
+            makeRequestResponse(req, req.res? req.res: socket);
             req.url = 'connect';
-            exp.handle(req, req.res);
+            exp.handle(req, req.res? req.res: socket);
 
             socket.on('disconnect', function(){
                 var req = socket.client.request;
-                makeRequestResponse(req,req.res);
+                makeRequestResponse(req, req.res? req.res: socket);
                 req.url = 'disconnect';
-                exp.handle(req, req.res);
+                exp.handle(req, req.res? req.res: socket);
             });
         });
 
